f071769716a256dbae262dd2ce9118f0b030c047
lkoshale <lkoshale@gmail.com>
2018-12-11 15:20:11 +0530

    migerated to project

--- sublime/Login.js
+++ sublime/Login.js
@@ -1,17 +1,8 @@
 function LogIN(){
-    var config = {
-        apiKey: "AIzaSyCsOUfZEBAhTB9HwaTxAlxBDEgw6R1O20s",
-        authDomain: "edar-v1.firebaseapp.com",
-        databaseURL: "https://edar-v1.firebaseio.com",
-        projectId: "edar-v1",
-        storageBucket: "<BUCKET>.appspot.com",
-        messagingSenderId: "<SENDER_ID>",
-    };
-    firebase.initializeApp(config);
 
     var id = document.getElementById("username").value ;
     var pd = document.getElementById("password").value ;
-    console.log(id+" "+pd);
+  //  console.log(id+" "+pd);
 
     if( id==="" || pd === ""){
         alert("Please fill username and password !!");
@@ -22,7 +13,7 @@ function LogIN(){
 
         console.log(typeof(root));
         for( var x in root){
-            console.log(x);
+           // console.log(x);
             var cid = root[x]['username'];
             var cpwd = root[x]['password'];
             console.log(cid+" "+cpwd);
@@ -31,7 +22,7 @@ function LogIN(){
                 sessionStorage.setItem('edarSCHOOLName', root[x]['name']);
                 // localStorage.setItem('edarSCHOOL', root[x]['username']);
                 // localStorage.setItem('edarSCHOOLName', root[x]['name']);
-                console.log("redirect");
+               // console.log("redirect");
                 window.location.href = "ViewData.html";
             }
         }
